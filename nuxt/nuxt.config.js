6e78e6852c098216476fedd7e3ced428df0aa524
Francisco Borja <fborja1414@gmail.com>
2021-06-28 18:33:14 -0700

    fix

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
   router: {
-    base: "/conne/"
+    base: "/<conne>/"
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
